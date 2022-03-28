28d7ea130be22689053d9aa9a5bd9dbe809081ea
htt315 <htt3152000@gmail.com>
2022-03-28 08:18:59 +0700

    api person

--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Person } from '../server_service/model/Person';
 import { PersonService } from '../server_service/Person/person.service';
 
 @Component({
@@ -9,27 +10,42 @@ import { PersonService } from '../server_service/Person/person.service';
 })
 export class PersonComponent implements OnInit {
 
-  person:FormGroup = new FormGroup({
+    updatePerson:FormGroup = new FormGroup({
     lastName: new FormControl('', Validators.required),
     firstName: new FormControl('', Validators.required),
     gender: new FormControl('', Validators.required),
     dateOfBirth: new FormControl('', Validators.required),
     phoneNumber: new FormControl(''),
-    idNumber: new FormControl( '', Validators.required),
+    idCard: new FormControl( '', Validators.required),
     email: new FormControl('', [Validators.required, Validators.email])
   })
-
+  person: any
   constructor(private personService: PersonService) { }
-
+personList: any
   ngOnInit(): void {
-  }
-
-  getPersonalInfoData(data: any) {
-    //COMMENT: the data here is the personal information that needs to be saved
-    console.warn(data);
-    this.personService.savePersonalInformation(data).subscribe((result) => {
-      console.warn(result);
+      this.personService.getAllPerson().subscribe((res: any) => {
+      this.personList = res
+      // show list person  
+      console.log(this.personList)
     })
   }
 
+   getPersonalInfoData(data: any) {
+    //COMMENT: the data here is the personal information that needs to be saved
+    /*  console.warn(data); */
+    this.personService.savePersonalInformation(data).subscribe((result) => {
+      
+      // show console value id 
+      /* console.log(result.id) */
+      sessionStorage.setItem('id', result.id.toString())
+    }) 
+    
+   /*  console.log(data)
+    this.personService.getAllPerson().subscribe(data =>{  
+      console.log(data);
+      this.person = data;  
+    } ) */
+  
+   
+}
 }
