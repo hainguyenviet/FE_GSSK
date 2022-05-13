5ab0c25e97e35feab0a9ee3e7f4147ad24119b71
lmphuc <lmphuc259@gmail.com>
2022-05-13 14:37:43 +0700

    Front-end Home Page

---
+++ src/app/carousel/carousel-sponsors/carousel-sponsors.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CarouselSponsorsComponent } from './carousel-sponsors.component';
+
+
+
+@NgModule({
+  declarations: [
+    CarouselSponsorsComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    CarouselSponsorsComponent
+  ]
+})
+export class CarouselSponsorsModule { }
