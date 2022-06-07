93bce597dd12fe8d014c44c491b75a9dafd7bfed
Phuclmp-sgt <phuclm.sgt@gmail.com>
2022-06-07 11:10:46 +0700

    fix ky hieu benh

--- src/app/input-information/input-information.component.ts
+++ src/app/input-information/input-information.component.ts
@@ -12,12 +12,11 @@ import { PersonService } from '../server_service/Person/person.service';
 export class InputInformationComponent implements OnInit {
   constructor(private fb: FormBuilder, private api: PersonService) {}
 
-  public inputForm !: FormGroup;
-  public personForm !:FormGroup;
+  public inputForm!: FormGroup;
+  public personForm!: FormGroup;
   public relatives_formGroup!: FormGroup;
-  public illnessForm !: FormGroup;
+  public illnessForm!: FormGroup;
   public addmore!: FormGroup;
-
 
   illNessGroup: any[] = [
     'Nhóm bệnh ung thư',
@@ -26,12 +25,40 @@ export class InputInformationComponent implements OnInit {
     'Nhóm bệnh nội thần kinh/tâm thần',
     'Khác',
   ];
-  ill: string[] = [
-    'Ung thư đại tràng',
-    'Parkinson',
-    'Rối loạn tăng động giảm chú ý',
-    'Động kinh',
+  illCancer: string[] = [
+    'Ung thư vú',
+    'Ung thư đại trực tràng',
+    'Ung thư dạ dày',
+    'Ung thư tử cung',
+    'Ung thư tuyến giáp',
+    'Ung thư máu',
+  ];
+
+  illHematology: string[] = [
+    'Rối loạn đông máu',
+    'Huyết khối tĩnh mạch sâu',
+    'Thuyên tắc phổi',
+    'Bệnh tăng cholesterol máu gia đình',
+  ];
+
+  illCardiovascular: string[] = [
+    'Nhồi máu cơ tim',
     'Rối loạn nhịp',
+    'Bệnh cơ tim giãn',
+    'Đau thắt ngực',
+  ];
+
+  illNeurological: string[] = [
+    'Rối loạn tâm thần',
+    'Động kinh',
+    'Rối loạn tăng động giảm chú ý',
+    'Tự kỷ',
+  ];
+
+  illOther: string[] = [
+    'Hen',
+    'Loãng xương',
+    'Đái tháo đường MODY',
   ];
 
 
@@ -79,22 +106,22 @@ export class InputInformationComponent implements OnInit {
     });
 
     this.illnessForm = this.fb.group({
-      isTwin:[''],
-      isAdopted: [''],
-      height:['', Validators.required],
+      isTwin: null,
+      isAdopted: null,
+      height: ['', Validators.required],
       weight: ['', Validators.required],
-      firstPeriodAge:[''],
-      birthControl: [''],
-      pregnantTime: [''],
-      firstBornAge:[''],
-      isSmoke: [''],
-      smokeTime:[''],
-      giveUpSmokeAge:[''],
-      wineVolume:[''],
-      workOutVolume:[''],
-      workOutType:[''],
+      firstPeriodAge: null,
+      birthControl: null,
+      pregnantTime: null,
+      firstBornAge: null,
+      isSmoke: null,
+      smokeTime: null,
+      giveUpSmokeAge: null,
+      wineVolume: null,
+      workOutVolume: null,
+      workOutType: null,
       illGroup: this.itemRows,
-    })
+    });
 
     this.personForm = this.fb.group({
       lastName: ['', Validators.required],
@@ -105,8 +132,11 @@ export class InputInformationComponent implements OnInit {
       idCard: ['', Validators.required],
       email: ['', Validators.required],
       healthRecord: this.illnessForm,
-      relativeList: this.relatives
-    })
+      relativeList: this.relatives,
+    });
+
+    this.initItemRows();
+    this.newRelative();
 
     
   }
@@ -115,16 +145,16 @@ export class InputInformationComponent implements OnInit {
     return this.addmore.controls['itemRows'] as FormArray;
   }
 
-  initItemRows() {
+  initItemRows() : FormGroup {
     return this.fb.group({
-      illGroup: [''],
-      illName: [''],
-      illAge: [''],
+      code: null,
+      illName: null,
+      name:'',
+      age_detected: null,
     });
   }
   addNewRow() {
     this.itemRows.push(this.initItemRows());
-    
   }
 
   deleteRow(index: number) {
@@ -138,14 +168,15 @@ export class InputInformationComponent implements OnInit {
   newRelative(): FormGroup {
     return this.fb.group({
       name: ['', Validators.required],
-      sex: ['', Validators.required],
-      idCard: [''],
+      gender: ['', Validators.required],
+      idCard: [null],
       relation: ['', Validators.required],
-      age: ['', Validators.required],
-      orderFamily: ['', Validators.required],
-      dead: [''],
-      deadAge: [''],
-      illNessRelative: this.fb.array([this.newIllNess()]),
+      age: [null, Validators.required],
+      familyOrder: [null],
+      isDead: [null],
+      dead_age: [null],
+      deathCause:[null],
+      relativeList: this.fb.array([this.newIllNess()]),
     });
   }
 
@@ -153,8 +184,7 @@ export class InputInformationComponent implements OnInit {
     this.index_of_relationship += 1;
     this.relatives.push(this.newRelative());
     if (
-      this.relatives.value[this.index_of_relationship - 1].relation ==
-        'Cậu' ||
+      this.relatives.value[this.index_of_relationship - 1].relation == 'Cậu' ||
       this.relatives.value[this.index_of_relationship - 1].relation == 'Cô' ||
       this.relatives.value[this.index_of_relationship - 1].relation == 'Dì' ||
       this.relatives.value[this.index_of_relationship - 1].relation == 'Chú'
@@ -183,7 +213,7 @@ export class InputInformationComponent implements OnInit {
       'Chị ruột' ||
       'Chị/em họ'
     ) {
-      this.relatives.value[relativeIndex].sex = 'Nữ';
+      this.relatives.value[relativeIndex].gender = 'Nữ';
     }
     if (
       value == 'Cha' ||
@@ -194,19 +224,113 @@ export class InputInformationComponent implements OnInit {
       value == 'Ông ngoại' ||
       value == 'Anh/em họ'
     ) {
-      this.relatives.value[relativeIndex].sex = 'Nam';
+      this.relatives.value[relativeIndex].gender = 'Nam';
     }
   }
+
+  selectIllNess(value: string, illNessIndex: number) {
+    if (value == 'Ung thư vú') 
+      this.itemRows.value[illNessIndex].name = 'BC1';
+    if (value == 'Ung thư tuyến giáp') 
+      this.itemRows.value[illNessIndex].name = 'TC';
+    if (value == 'Ung thư máu') 
+      this.itemRows.value[illNessIndex].name = 'BC2';
+    if (value == 'Ung thư tử cung') 
+      this.itemRows.value[illNessIndex].name = 'UC';
+    if (value == 'Ung thư dạ dày') 
+      this.itemRows.value[illNessIndex].name = 'SC1';
+    if (value == 'Ung thư đại trực tràng') 
+      this.itemRows.value[illNessIndex].name = 'CC';
+    if (value == 'Rối loạn đông máu')
+      this.itemRows.value[illNessIndex].name = 'BCD';
+    if (value == 'Huyết khối tĩnh mạch sâu') 
+      this.itemRows.value[illNessIndex].name = 'DVT';
+    if (value == 'Thuyên tắc phổi') 
+      this.itemRows.value[illNessIndex].name = 'PE';
+    if (value == 'Bệnh tăng cholesterol máu gia đình') 
+      this.itemRows.value[illNessIndex].name = 'FH';
+    if (value == 'Nhồi máu cơ tim') 
+      this.itemRows.value[illNessIndex].name = 'MI';
+    if (value == 'Rối loạn nhịp') 
+      this.itemRows.value[illNessIndex].name = 'CA2';
+    if (value == 'Bệnh cơ tim giãn') 
+      this.itemRows.value[illNessIndex].name = 'DCM';
+    if (value == 'Đau thắt ngực')
+      this.itemRows.value[illNessIndex].name = 'AP';
+    if (value == 'Rối loạn tâm thần') 
+      this.itemRows.value[illNessIndex].name = 'P';
+    if (value == 'Động kinh') 
+      this.itemRows.value[illNessIndex].name = 'E';
+    if (value == 'Rối loạn tăng động giảm chú ý') 
+      this.itemRows.value[illNessIndex].name = 'ADHD';
+    if (value == 'Tự kỷ') 
+      this.itemRows.value[illNessIndex].name = 'A1';
+    if (value == 'Hen') 
+      this.itemRows.value[illNessIndex].name = 'A2';
+    if (value == 'Loãng xương') 
+      this.itemRows.value[illNessIndex].name = 'O';
+    if (value == 'Đái tháo đường MODY')
+      this.itemRows.value[illNessIndex].name = 'MODY';
+  }
+
+
+
   illNessList(empIndex: number): FormArray {
-    return this.relatives.at(empIndex).get('illNessRelative') as FormArray;
+    return this.relatives.at(empIndex).get('relativeList') as FormArray;
   }
 
   newIllNess(): FormGroup {
     return this.fb.group({
-      illGroup: [''],
+      code: [null],
       illName: [''],
-      illAge: [''],
+      name:[''],
+      age_detected: [null],
     });
+  }
+
+  selectIllNessRelative(value: string, illNessIndex: number) {
+    if (value == 'Ung thư vú') 
+      this.illNessList(illNessIndex).value[illNessIndex].name = 'BC1';
+    if (value == 'Ung thư tuyến giáp') 
+      this.illNessList(illNessIndex).value[illNessIndex].name = 'TC';
+    if (value == 'Ung thư máu') 
+      this.illNessList(illNessIndex).value[illNessIndex].name = 'BC2';
+    if (value == 'Ung thư tử cung') 
+      this.illNessList(illNessIndex).value[illNessIndex].name = 'UC';
+    if (value == 'Ung thư dạ dày') 
+      this.illNessList(illNessIndex).value[illNessIndex].name = 'SC1';
+    if (value == 'Ung thư đại trực tràng') 
+      this.illNessList(illNessIndex).value[illNessIndex].name = 'CC';
+    if (value == 'Rối loạn đông máu')
+      this.illNessList(illNessIndex).value[illNessIndex].name = 'BCD';
+    if (value == 'Huyết khối tĩnh mạch sâu') 
+      this.illNessList(illNessIndex).value[illNessIndex].name = 'DVT';
+    if (value == 'Thuyên tắc phổi') 
+      this.illNessList(illNessIndex).value[illNessIndex].name = 'PE';
+    if (value == 'Bệnh tăng cholesterol máu gia đình') 
+      this.illNessList(illNessIndex).value[illNessIndex].name = 'FH';
+    if (value == 'Nhồi máu cơ tim') 
+      this.illNessList(illNessIndex).value[illNessIndex].name = 'MI';
+    if (value == 'Rối loạn nhịp') 
+      this.illNessList(illNessIndex).value[illNessIndex].name = 'CA2';
+    if (value == 'Bệnh cơ tim giãn') 
+      this.illNessList(illNessIndex).value[illNessIndex].name = 'DCM';
+    if (value == 'Đau thắt ngực')
+      this.illNessList(illNessIndex).value[illNessIndex].name = 'AP';
+    if (value == 'Rối loạn tâm thần') 
+      this.illNessList(illNessIndex).value[illNessIndex].name = 'P';
+    if (value == 'Động kinh') 
+      this.illNessList(illNessIndex).value[illNessIndex].name = 'E';
+    if (value == 'Rối loạn tăng động giảm chú ý') 
+      this.illNessList(illNessIndex).value[illNessIndex].name = 'ADHD';
+    if (value == 'Tự kỷ') 
+      this.illNessList(illNessIndex).value[illNessIndex].name = 'A1';
+    if (value == 'Hen') 
+      this.illNessList(illNessIndex).value[illNessIndex].name = 'A2';
+    if (value == 'Loãng xương') 
+      this.illNessList(illNessIndex).value[illNessIndex].name = 'O';
+    if (value == 'Đái tháo đường MODY')
+      this.illNessList(illNessIndex).value[illNessIndex].name = 'MODY';
   }
 
   addNewRowIllRelative(relativeIndex: number) {
@@ -217,29 +341,23 @@ export class InputInformationComponent implements OnInit {
     this.illNessList(empIndex).removeAt(illNessIndex);
   }
 
-
-
-
   postPerson() {
-
-    if(this.personForm.valid){
-      this.api.postPerson(this.personForm.value)
-      .subscribe({
-        next:(res)=>{
-          sessionStorage.setItem('idUser', res.id.toString())
-          this.api.convertGenogram(sessionStorage.getItem('idUser')!)
-          alert('Person added successfully')
+    if (this.personForm.valid) {
+      this.api.postPerson(this.personForm.value).subscribe({
+        next: (res) => {
+          sessionStorage.setItem('idUser', res.id.toString());
+          this.api.convertGenogram(sessionStorage.getItem('idUser')!);
+          alert('Person added successfully');
           this.personForm.reset();
         },
-        error:()=>{
-          alert("Error")
-        }
-      })
-    }else{
-      alert('Hãy Điền Đầy Đủ Thông Tin Cần Thiết')
+        error: () => {
+          alert('Error');
+        },
+      });
+    } else {
+      alert('Hãy Điền Đầy Đủ Thông Tin Cần Thiết');
     }
   }
-
 
   goNext(progress: ProgressComponent) {
     progress.next();
@@ -252,5 +370,4 @@ export class InputInformationComponent implements OnInit {
   onStateChange(event: any) {
     console.log(event);
   }
-
 }
