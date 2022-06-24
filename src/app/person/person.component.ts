41dc250f1f1a1165a2362ca9d35b8882a2bce435
htt315 <htt3152000@gmail.com>
2022-06-25 04:17:34 +0700

    responsive

--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import {
+  FormGroup,
+  Validators,
+  FormBuilder,
+  FormControl,
+} from '@angular/forms';
 import { PersonService } from '../server_service/Person/person.service';
 import { HttpErrorResponse } from '@angular/common/http';
 
@@ -9,49 +14,68 @@ import { HttpErrorResponse } from '@angular/common/http';
   styleUrls: ['./person.component.scss'],
 })
 export class PersonComponent implements OnInit {
-  personForm !: FormGroup 
+  personForm!: FormGroup;
   person: any;
-  constructor(private api: PersonService,private _fb: FormBuilder) {}
-  personList: any;
-  ngOnInit(){
-    this.personForm = this._fb.group({
-    lastName: ['', Validators.required],
-    firstName:  ['', Validators.required],
-    gender:  ['', Validators.required],
-    dateOfBirth:  ['', Validators.required],
-    phoneNumber:  ['', Validators.required],
-    idCard:  ['', Validators.required],
-    email:  ['', Validators.required],
-    }) 
 
+  constructor(private api: PersonService, private _fb: FormBuilder) {}
+  personList: any;
+  get nameError() {
+    if (this.personForm.get('firstName')?.hasError('required'))
+      return 'Name field is required';
+
+    if (this.personForm.get('firstName')?.hasError('whitespace'))
+      return 'Please enter a valid Name';
+    else return '';
+  }
+  get name() {
+    return this.personForm.get('firstName') as FormControl;
+  }
+  ngOnInit() {
+    this.personForm = this._fb.group({
+      lastName: ['', Validators.required],
+      firstName: ['', Validators.required],
+      gender: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      phoneNumber: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('^[0-9]*$'),
+          Validators.minLength(10),
+        ],
+      ],
+      idCard: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+    });
     this.getAllPerson();
     //console.log(this.updatePerson.value)
   }
-  
+  get f() {
+    return this.personForm.controls;
+  }
   public getAllPerson(): void {
-    this.api.getAllPerson().subscribe((res: any) => {
-      this.personList = res;
-      // show list person
-      //console.log(this.personList)
-    },
+    this.api.getAllPerson().subscribe(
+      (res: any) => {
+        this.personList = res;
+        // show list person
+        //console.log(this.personList)
+      },
       (error: HttpErrorResponse) => {
         alert(error.message);
       }
-      )
+    );
   }
   postPerson() {
-    if(this.personForm.valid){
-      this.api.postPerson(this.personForm.value)
-      .subscribe({
-        next:(res)=>{
-          alert('Person added successfully')
+    if (this.personForm.valid) {
+      this.api.postPerson(this.personForm.value).subscribe({
+        next: (res) => {
+          alert('Person added successfully');
           this.personForm.reset();
         },
-        error:()=>{
-          alert("Error")
-        }
-      })
+        error: () => {
+          alert('Error');
+        },
+      });
     }
   }
-
 }
