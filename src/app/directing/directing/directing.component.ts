a5861493653516aaaee71d0737e88edd9ef9b7f5
baophan1508 <phnbao18@apcs.vn>
2022-08-18 15:58:29 +0700

    update for login google role

--- src/app/directing/directing/directing.component.ts
+++ src/app/directing/directing/directing.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import jwt_decode from 'jwt-decode'
 @Component({
   selector: 'app-directing',
   templateUrl: './directing.component.html',
@@ -12,13 +13,26 @@ export class DirectingComponent implements OnInit {
   constructor(private router: Router, private activatedRoute: ActivatedRoute) {
     
    }
-
+   getDecodedAccessToken(token: string): any {
+    try {
+      return jwt_decode(token);
+    } catch(Error) {
+      return null;
+    }
+  }
   ngOnInit(): void {
  
     this.activatedRoute.params.subscribe(params => {   
       localStorage.setItem('access_token', params.token)
       localStorage.setItem('username', params.email)
-      this.router.navigateByUrl('/input-information')
+      const tokenInfo = this.getDecodedAccessToken(params.token)
+      const role = tokenInfo.role[0]
+      localStorage.setItem('ROLE', role)
+      if (role === "USER")
+      {
+        this.router.navigateByUrl('/input-information')
+      }
+      
     });
   
   }
